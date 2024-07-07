5200b5e9c2ebf4cc2f8e0b3816966e3456422ad7
riffaaznur <143594507+riffaaznur@users.noreply.github.com>
2024-07-08 01:11:26 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-        const menu = document.querySelector('.menu');
-
-        hamburger.addEventListener('click', () => {
-            menu.classList.toggle('show');
-        });
